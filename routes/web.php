0d858182f07d837cb83a7c46f084586b4d15a8aa
lebinh23091999@gmail.com <lebinh23091999@gmail.com>
2023-04-04 21:14:14 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('ok', function(){
+Route::get('okela', function(){
     return 'doneeeee';
 });
