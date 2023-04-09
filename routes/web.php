b1e427d7b95d9c5e32f9061ec5b9577226ce5ff0
lebinh23091999@gmail.com <lebinh23091999@gmail.com>
2023-04-09 15:29:41 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('jenkin', function(){
+
+Route::get('jenkin', function () {
     return 'jenkin';
 });
